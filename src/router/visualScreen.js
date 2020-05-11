9b18f23a0bf3fea734b48f4dc93b54b802258afd
yang3406 <1158890486@qq.com>
2020-05-11 10:21:03 +0800

    delete serve code

--- src/router/visualScreen.js
+++
@@ -1,9 +0,0 @@
-export default [{
-  path: '/visualScreen/content',
-  component: () => import('../views/visualScreen/content/index')
-},
-{
-  path: '/visualScreen/content/edit',
-  component: () => import('../views/visualScreen/content/editor/index')
-}
-]
